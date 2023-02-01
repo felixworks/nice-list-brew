88a6a6c602a0bdbb03b89278126e377d40065401
Felix Meyer <phelixm@gmail.com>
2023-01-31 22:27:24 -0600

    display brewery data and create links

--- src/components/Brewery/Brewery.js
+++ src/components/Brewery/Brewery.js
@@ -1,5 +1,23 @@
-function Brewery({breweryData}) {
-  return <li className={breweryData.id}>{breweryData.name}</li>;
+function Brewery({ breweryData }) {
+  return (
+    <li className={breweryData.id}>
+      <h3>
+        <a href={breweryData.website_url} target="_blank">
+          {breweryData.name}
+        </a>
+      </h3>
+      <div>Brewery Type: {breweryData.brewery_type}</div>
+      <div>
+        <a
+          href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+            breweryData.name
+          )}%20${encodeURIComponent(breweryData.street)}`}
+        >
+          {breweryData.city}
+        </a>
+      </div>
+    </li>
+  );
 }
 
 export default Brewery;
