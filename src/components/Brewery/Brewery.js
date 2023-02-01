fd706902a80eff4040a41f6b76a9fe7096e2f7b6
Felix Meyer <phelixm@gmail.com>
2023-01-31 22:59:11 -0600

    add, edit, and delete functionality

--- src/components/Brewery/Brewery.js
+++ src/components/Brewery/Brewery.js
@@ -1,21 +1,86 @@
-function Brewery({ breweryData }) {
+import React, { useState } from "react";
+
+function Brewery({ breweryData, updateBrewery, deleteBrewery }) {
+  const [editing, setEditing] = useState(false);
+  const [formData, setFormData] = useState({
+    id: breweryData.id,
+    name: breweryData.name,
+    brewery_type: breweryData.brewery_type,
+    city: breweryData.city,
+    street: breweryData.street,
+    website_url: breweryData.website_url,
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateBrewery(formData);
+    setEditing(false);
+  };
+
   return (
     <li className={breweryData.id}>
-      <h3>
-        <a href={breweryData.website_url} target="_blank">
-          {breweryData.name}
-        </a>
-      </h3>
-      <div>Brewery Type: {breweryData.brewery_type}</div>
-      <div>
-        <a
-          href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-            breweryData.name
-          )}%20${encodeURIComponent(breweryData.street)}`}
-        >
-          {breweryData.city}
-        </a>
-      </div>
+      {editing ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="brewery_type"
+            value={formData.brewery_type}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="street"
+            value={formData.street}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="website_url"
+            value={formData.website_url}
+            onChange={handleChange}
+          />
+          <button type="submit">Save</button>
+        </form>
+      ) : (
+        <>
+          <h3>
+            <a href={breweryData.website_url} target="_blank">
+              {breweryData.name}
+            </a>
+          </h3>
+          <div>Brewery Type: {breweryData.brewery_type}</div>
+          <div>
+            <a
+              href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+                breweryData.name
+              )}%20${encodeURIComponent(breweryData.street)}`}
+            >
+              {breweryData.city}
+            </a>
+          </div>
+          <button onClick={() => setEditing(true)}>Edit</button>
+          <button onClick={() => deleteBrewery(breweryData.id)}>Delete</button>
+        </>
+      )}
     </li>
   );
 }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { API_BASE_URL } from "../../config.js";
+import { v4 as uuidv4 } from "uuid";
 import Brewery from "../Brewery/Brewery.js";
 
 function List() {
@@ -18,14 +19,44 @@ function List() {
     fetchBreweries();
   }, []);
   console.log("state of breweries", breweries);
+
+  const addBrewery = (newBrewery) => {
+    setBreweries([...breweries, newBrewery]);
+  };
+
+  const updateBrewery = (updatedBrewery) => {
+    console.log("updating brewery", updatedBrewery);
+    setBreweries(
+      breweries.map((brewery) =>
+        brewery.id === updatedBrewery.id ? {...brewery, ...updatedBrewery} : brewery
+      )
+    );
+  };
+
+  const deleteBrewery = (id) => {
+    setBreweries(breweries.filter((brewery) => brewery.id !== id));
+  };
+
   return (
-    <ul>
-      {breweries.length > 0
-        ? breweries.map((item, index) => {
-            return <Brewery key={item.id} breweryData={item} />;
-          })
-        : "No results"}
-    </ul>
+    <>
+      <ul>
+        {breweries.length > 0
+          ? breweries.map((item, index) => {
+              return (
+                <Brewery
+                  key={item.id}
+                  breweryData={item}
+                  updateBrewery={updateBrewery}
+                  deleteBrewery={deleteBrewery}
+                />
+              );
+            })
+          : "No results"}
+      </ul>
+      <button onClick={() => addBrewery({ id: uuidv4(), name: "New Brewery" })}>
+        Add Brewery
+      </button>
+    </>
   );
 }
 
